c6afd6485b714f40ab2f8d2d94f9d1bf79f13cff
mrOrange <35071573+mrOranger@users.noreply.github.com>
2023-02-08 11:57:27 +0100

    Initial commit

---
+++ backend/src/main/java/it/pharmacywebassistant/PharmacyWebAssistantApplication.java
@@ -0,0 +1,13 @@
+package it.pharmacywebassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyWebAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyWebAssistantApplication.class, args);
+	}
+
+}
