68e186414ad21485ebd8a0935702492a623a9bb4
mrOrange <35071573+mrOranger@users.noreply.github.com>
2023-02-12 13:12:06 +0100

    Complete Swagger Documentation for CosmeticController

--- backend/src/main/java/it/pharmacywebassistant/controller/CosmeticController.java
+++ backend/src/main/java/it/pharmacywebassistant/controller/CosmeticController.java
@@ -1,11 +1,19 @@
 package it.pharmacywebassistant.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import it.pharmacywebassistant.controller.exception.BadRequestException;
 import it.pharmacywebassistant.controller.exception.ConflictException;
 import it.pharmacywebassistant.controller.exception.NotFoundException;
 import it.pharmacywebassistant.controller.message.Message;
 import it.pharmacywebassistant.model.Cosmetic;
 import it.pharmacywebassistant.model.dto.CosmeticDTO;
+import it.pharmacywebassistant.model.dto.DrugDTO;
 import it.pharmacywebassistant.service.CosmeticService;
 import jakarta.validation.Valid;
 import lombok.SneakyThrows;
@@ -25,6 +33,7 @@ import java.util.stream.Stream;
 
 @RestController
 @RequestMapping(path = "v1/api/products/cosmetics", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "CosmeticController", description = "Controller per le operazioni riguardo un Cosmetico")
 public class CosmeticController {
 
     @Autowired
@@ -33,6 +42,15 @@ public class CosmeticController {
     @Autowired
     private ResourceBundleMessageSource errorMessage;
 
+    @Operation(summary = "GET all Cosmetics", description = "Restituisce tutti i Cosmetici presenti nel Database", tags = "Get")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sono stati trovati dei Cosmetici nel Database", content = {
+                    @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = CosmeticDTO.class)))
+            }),
+            @ApiResponse(responseCode = "404", description = "Non è stato trovato alcun Cosmetico nel Database", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            })
+    })
     @GetMapping(path = "/") @SneakyThrows
     public ResponseEntity<List<CosmeticDTO>> getAllCosmetics() {
         final List<CosmeticDTO> products = service.findAll();
@@ -42,6 +60,15 @@ public class CosmeticController {
         return ResponseEntity.ok(products);
     }
 
+    @Operation(summary = "GET Cosmetic by Id", description = "Restituisce il cosmetico corrispondente all'Id passato in input", tags = "Get")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "E'stato trovato un Cosmetico all'interno del Database", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CosmeticDTO.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Non è stato trovato alcun Cosmetico all'interno del Database", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            })
+    })
     @GetMapping(path = "/{id}") @SneakyThrows
     public ResponseEntity<CosmeticDTO> getCosmeticById(@PathVariable Long id) {
         final Optional<CosmeticDTO> product = service.findById(id);
@@ -51,6 +78,18 @@ public class CosmeticController {
         return ResponseEntity.ok(product.get());
     }
 
+    @Operation(summary = "POST new Cosmetic", description = "Inserisce un nuovo Cosmetico nel Database", tags = "Post")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Il nuovo Cosmetico è stato inserito con successo", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Il formato della richiesta non è valido", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            }),
+            @ApiResponse(responseCode = "409", description = "Il Cosmetico è già registrato, usare il metodo PUT per modificarlo", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            })
+    })
     @PostMapping(path = "/", consumes = MediaType.APPLICATION_JSON_VALUE) @SneakyThrows
     public ResponseEntity<Message> postCosmetic(@Valid @RequestBody Cosmetic product, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
@@ -65,6 +104,18 @@ public class CosmeticController {
         return ResponseEntity.ok(new Message(LocalDate.now(), HttpStatus.OK.value(), "Prodotto inserito con successo!"));
     }
 
+    @Operation(summary = "PUT a Cosmetic", description = "Modifica un Cosmetico registrato nel Database", tags = "Put")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Il Cosmetico è stato registrato con successo", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Il formato della richiesta non è valido", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Il Cosmetico non è registrato nel Database, usare il metodo POST per inserirlo", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            })
+    })
     @PutMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE) @SneakyThrows
     public ResponseEntity<Message> putCosmetic(@PathVariable Long id, @Valid @RequestBody Cosmetic product, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
@@ -80,6 +131,15 @@ public class CosmeticController {
         return ResponseEntity.ok(new Message(LocalDate.now(), HttpStatus.OK.value(), "Prodotto modificato con successo!"));
     }
 
+    @Operation(summary = "DELETE all Cosmetics", description = "Elimina tutti i Cosmetici registrati nel Database", tags = "Delete")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "I Cosmetici sono stati eliminati correttamente", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            }),
+            @ApiResponse(responseCode = "409", description = "Non esiste alcun Cosmetico registrato nel Database", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            })
+    })
     @DeleteMapping(path = "/") @SneakyThrows
     public ResponseEntity<Message> deleteAllProducts() {
         final List<CosmeticDTO> products = service.findAll();
@@ -90,6 +150,15 @@ public class CosmeticController {
         return ResponseEntity.ok(new Message(LocalDate.now(), HttpStatus.OK.value(), "Prodotti eliminati con successo!"));
     }
 
+    @Operation(summary = "DELETE a Cosmetic by Id", description = "Elimina il Cosmetico identificato dall'Id passato come parametro", tags = "Delete")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Il Cosmetico è stato registrato correttamente", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            }),
+            @ApiResponse(responseCode = "409", description = "Non esiste alcun cosmetico registrato nel Database, oppure con l'Id passato come parametro", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            })
+    })
     @DeleteMapping(path = "/{id}") @SneakyThrows
     public ResponseEntity<Message> deleteProduct(@PathVariable Long id) {
         final List<CosmeticDTO> products = service.findAll();
