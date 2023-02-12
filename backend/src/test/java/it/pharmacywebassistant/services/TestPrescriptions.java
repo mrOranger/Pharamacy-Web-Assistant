0d2a5b2cff0dffb48e4bdd2d3005128bb3a16ea0
mrOrange <35071573+mrOranger@users.noreply.github.com>
2023-02-12 22:05:36 +0100

    Defined test cases for Prescription

---
+++ backend/src/test/java/it/pharmacywebassistant/services/TestPrescriptions.java
@@ -0,0 +1,57 @@
+package it.pharmacywebassistant.services;
+
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public final class TestPrescriptions {
+
+    public void beforeAll() {
+    }
+
+    public void testGetAllPrescriptionsReturnsArrayOfSizeZero() {
+
+    }
+
+    public void testGetPrescriptionByIdReturnsEmptyOptional() {
+
+    }
+
+    public void testSaveNewPrescriptionReturnsANewPrescriptionWithIdOne() {
+
+    }
+
+    public void testGetAllPrescriptionsReturnsArrayContainingPrescriptionWithIdOne() {
+
+    }
+
+    public void testGetPrescriptionByIdReturnsOptionalWithPrescriptionWithIdOne() {
+
+    }
+
+    public void testSavePrescriptionReturnsAPrescriptionWithIdTwo() {
+
+    }
+
+    public void testGetAllPrescriptionsReturnsArrayContainingPrescriptionWithIdTwo() {
+
+    }
+
+    public void testGetPrescriptionByIdReturnsOptionalWithPrescriptionWithIdTwo() {
+
+    }
+
+    public void testDeletePrescriptionWithIdTwo() {
+
+    }
+
+    public void testGetAllPrescriptionsReturnsArrayOfSizeZeroAgain() {
+
+    }
+
+    public void testGetPrescriptionByIdReturnsEmptyOptionalAgain() {
+
+    }
+}
