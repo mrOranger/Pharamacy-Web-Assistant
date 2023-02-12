21ce4e360fb9b2713fe2932d67347926a05c4201
mrOrange <35071573+mrOranger@users.noreply.github.com>
2023-02-12 21:59:45 +0100

    Removed not used Repository and Services for Person

--- backend/src/main/java/it/pharmacywebassistant/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package it.pharmacywebassistant.repository;
-
-import it.pharmacywebassistant.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, String> {
-}
--- backend/src/main/java/it/pharmacywebassistant/service/implementation/PersonServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package it.pharmacywebassistant.service.implementation;
-
-import it.pharmacywebassistant.service.PersonService;
-
-public class PersonServiceImpl implements PersonService {
-}
