9176c21d2ef7128566e7514d77c1222c23297e04
mrOrange <35071573+mrOranger@users.noreply.github.com>
2023-02-14 16:16:57 +0100

    Add final modifier to class definition

--- backend/src/test/java/it/pharmacywebassistant/services/TestDrugService.java
+++ backend/src/test/java/it/pharmacywebassistant/services/TestDrugService.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class TestDrugService {
+public final class TestDrugService {
 
     @Autowired
     private ProductService service;
