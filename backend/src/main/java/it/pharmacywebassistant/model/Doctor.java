a3bf49c9575683b1c941c633bd364ec63e57e020
mrOrange <35071573+mrOranger@users.noreply.github.com>
2023-02-14 16:57:04 +0100

    Created reference OneToMany between Doctor and Prescription

--- backend/src/main/java/it/pharmacywebassistant/model/Doctor.java
+++ backend/src/main/java/it/pharmacywebassistant/model/Doctor.java
@@ -1,14 +1,16 @@
 package it.pharmacywebassistant.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity(name = "Doctor") @Getter @Setter @AllArgsConstructor
@@ -17,7 +19,12 @@ public final class Doctor extends Person implements Serializable {
     @Serial
     private static final long serialVersionUID = -9284711738271L;
 
+    @OneToMany(mappedBy = "doctor")
+    @JsonManagedReference
+    private List<Prescription> prescriptionList;
+
     public Doctor(String taxCode, String firstName, String lastName, Date dateOfBirth) {
         super(taxCode, firstName, lastName, dateOfBirth);
+        this.prescriptionList = new ArrayList<>();
     }
 }
