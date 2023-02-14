a524c6b07c883ad5a98e9965fdd8e74e099e5ccb
mrOrange <35071573+mrOranger@users.noreply.github.com>
2023-02-14 16:30:38 +0100

    Complete definition of Swagger for Doctor and Patient Controllers

--- backend/src/main/java/it/pharmacywebassistant/controller/DoctorController.java
+++ backend/src/main/java/it/pharmacywebassistant/controller/DoctorController.java
@@ -1,5 +1,12 @@
 package it.pharmacywebassistant.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import it.pharmacywebassistant.controller.exception.BadRequestException;
 import it.pharmacywebassistant.controller.exception.ConflictException;
@@ -35,6 +42,15 @@ public final class DoctorController {
     @Autowired
     private ResourceBundleMessageSource errorMessage;
 
+    @Operation(summary = "GET Doctors", description = "Restituisce tutti i Dottori registrati nel Database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sono presenti dei Dottori all'interno del Database", content = {
+                    @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = PersonDTO.class)))
+            }),
+            @ApiResponse(responseCode = "404", description = "Non sono stati trovati dei Dottori all'interno del Database", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            })
+    })
     @GetMapping(path = "/") @SneakyThrows
     public ResponseEntity<List<PersonDTO>> getAllDoctors() {
         final List<PersonDTO> patientDTOList = service.findAllDoctors();
@@ -44,8 +60,17 @@ public final class DoctorController {
         return ResponseEntity.ok(patientDTOList);
     }
 
+    @Operation(summary = "GET Doctor by Tax Code", description = "Restituisce il Dottore corrispondente al Codice Fiscale passato come parametro")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "E'stato trovato un Dottore corrispondente al Codice Fiscale come parametro.", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = PersonDTO.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Non è stato trovato alcun Dottore con Codice Fiscale corrispondente al valore passato come parametro.", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            })
+    })
     @GetMapping(path = "/{taxCode}") @SneakyThrows
-    public ResponseEntity<PersonDTO> getDoctorByTaxCode(@PathVariable String taxCode) {
+    public ResponseEntity<PersonDTO> getDoctorByTaxCode(@Parameter(description = "Codice Fiscale del Dottore da cercare") @PathVariable String taxCode) {
         final Optional<PersonDTO> doctor = service.findByTaxCode(taxCode);
         if(doctor.isEmpty() || !(doctor.get() instanceof DoctorDTO)) {
             throw new NotFoundException("Nessun Dottore registrato con Codice Fiscale " + taxCode + "!");
@@ -53,8 +78,20 @@ public final class DoctorController {
         return ResponseEntity.ok(doctor.get());
     }
 
+    @Operation(summary = "POST new Doctor", description = "Inserisce un nuovo Dottore all'interno del Database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Il nuovo Dottore è stato inserito correttamente.", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Il formato della richiesta non è valido", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            }),
+            @ApiResponse(responseCode = "409", description = "Il Dottore è già registrato, usare il metodo PUT per modificarlo", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            })
+    })
     @PostMapping(path = "/", consumes = MediaType.APPLICATION_JSON_VALUE) @SneakyThrows
-    public ResponseEntity<Message> postDoctor(@Valid @RequestBody Doctor doctor, BindingResult bindingResult) {
+    public ResponseEntity<Message> postDoctor(@Parameter(description = "Paziente da registrare") @Valid @RequestBody Doctor doctor, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
             System.out.println(errorMessage.getMessage(bindingResult.getFieldError(), LocaleContextHolder.getLocale()));
             throw new BadRequestException(errorMessage.getMessage(bindingResult.getFieldError(), LocaleContextHolder.getLocale()));
@@ -67,8 +104,20 @@ public final class DoctorController {
         return ResponseEntity.ok(new Message(LocalDate.now(), HttpStatus.OK.value(), "Dottore registrato con successo!"));
     }
 
+    @Operation(summary = "PUT a Doctor", description = "Modifica un Dottore registrato nel Database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Il Dottore è stato modificato correttamente", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Il formato della richiesta non è valido", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Il Dottore non è registrato, usare il metodo POST per inserirlo", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            })
+    })
     @PutMapping(path = "/{taxCode}", consumes = MediaType.APPLICATION_JSON_VALUE) @SneakyThrows
-    public ResponseEntity<Message> putDoctor(@PathVariable String taxCode, @Valid @RequestBody Doctor doctor, BindingResult bindingResult) {
+    public ResponseEntity<Message> putDoctor(@Parameter(description = "Codice Fiscale del Dottore da modificare") @PathVariable String taxCode, @Parameter(description = "Dottore da modificare") @Valid @RequestBody Doctor doctor, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
             System.out.println(errorMessage.getMessage(bindingResult.getFieldError(), LocaleContextHolder.getLocale()));
             throw new BadRequestException(errorMessage.getMessage(bindingResult.getFieldError(), LocaleContextHolder.getLocale()));
@@ -82,8 +131,17 @@ public final class DoctorController {
         return ResponseEntity.ok(new Message(LocalDate.now(), HttpStatus.OK.value(), "Dottore modificato con successo!"));
     }
 
+    @Operation(summary = "DELETE a Patient by Tax Code", description = "Elimina un Dottore corrispondente al Codice Fiscale passato come parametro")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Il Dottore è stato eliminato correttamente", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            }),
+            @ApiResponse(responseCode = "409", description = "Non esiste alcun Dottore quel Codice Fiscale registrato nel Database", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            })
+    })
     @DeleteMapping(path = "/{taxCode}", consumes = MediaType.APPLICATION_JSON_VALUE) @SneakyThrows
-    public ResponseEntity<Message> deleteDoctorByTaxCode(@PathVariable  String taxCode) {
+    public ResponseEntity<Message> deleteDoctorByTaxCode(@Parameter(description = "Codice Fiscale del Dottore da rimuovere") @PathVariable  String taxCode) {
         final Optional<PersonDTO> doctorDTO = service.findByTaxCode(taxCode);
         if (doctorDTO.isEmpty() || !(doctorDTO.get() instanceof DoctorDTO)) {
             throw new ConflictException("Dottore non registrato, usare metodo POST per registrarlo!");
--- backend/src/main/java/it/pharmacywebassistant/controller/PatientController.java
+++ backend/src/main/java/it/pharmacywebassistant/controller/PatientController.java
@@ -1,5 +1,12 @@
 package it.pharmacywebassistant.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import it.pharmacywebassistant.controller.exception.BadRequestException;
 import it.pharmacywebassistant.controller.exception.ConflictException;
@@ -8,6 +15,7 @@ import it.pharmacywebassistant.controller.message.Message;
 import it.pharmacywebassistant.model.Patient;
 import it.pharmacywebassistant.model.dto.PatientDTO;
 import it.pharmacywebassistant.model.dto.PersonDTO;
+import it.pharmacywebassistant.model.dto.ProductDTO;
 import it.pharmacywebassistant.service.PersonService;
 import jakarta.validation.Valid;
 import lombok.SneakyThrows;
@@ -35,6 +43,15 @@ public final class PatientController {
     @Autowired
     private ResourceBundleMessageSource errorMessage;
 
+    @Operation(summary = "GET Patients", description = "Restituisce tutti i Pazienti registrati nel Database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sono presenti dei Pazienti all'interno del Database", content = {
+                    @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = PersonDTO.class)))
+            }),
+            @ApiResponse(responseCode = "404", description = "Non sono stati trovati dei Pazienti all'interno del Database", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            })
+    })
     @GetMapping(path = "/") @SneakyThrows
     public ResponseEntity<List<PersonDTO>> getAllPatients() {
         final List<PersonDTO> patientDTOList = service.findAllPatients();
@@ -44,8 +61,17 @@ public final class PatientController {
         return ResponseEntity.ok(patientDTOList);
     }
 
+    @Operation(summary = "GET Patient by Tax Code", description = "Restituisce il Paziente corrispondente al Codice Fiscale  passato come parametro")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "E'stato trovato un Paziente corrispondente all'id passato come parametro.", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = PersonDTO.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Non è stato trovato alcun Paziente con il Codice Fiscale corrispondente al valore passato come parametro.", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            })
+    })
     @GetMapping(path = "/{taxCode}") @SneakyThrows
-    public ResponseEntity<PersonDTO> getPatientByTaxCode(@PathVariable String taxCode) {
+    public ResponseEntity<PersonDTO> getPatientByTaxCode(@Parameter(description = "Codice Fiscale del Paziente da cercare") @PathVariable String taxCode) {
         final Optional<PersonDTO> patient = service.findByTaxCode(taxCode);
         if(patient.isEmpty() || !(patient.get() instanceof PatientDTO)) {
             throw new NotFoundException("Nessun paziente registrato con Codice Fiscale " + taxCode + "!");
@@ -53,8 +79,20 @@ public final class PatientController {
         return ResponseEntity.ok(patient.get());
     }
 
+    @Operation(summary = "POST new Patient", description = "Inserisce un nuovo Paziente all'interno del Database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Il nuovo Paziente è stato inserito correttamente.", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Il formato della richiesta non è valido", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            }),
+            @ApiResponse(responseCode = "409", description = "Il Paziente è già registrato, usare il metodo PUT per modificarlo", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            })
+    })
     @PostMapping(path = "/", consumes = MediaType.APPLICATION_JSON_VALUE) @SneakyThrows
-    public ResponseEntity<Message> postPatient(@Valid @RequestBody Patient patient, BindingResult bindingResult) {
+    public ResponseEntity<Message> postPatient(@Parameter(description = "Paziente da inserire") @Valid @RequestBody Patient patient, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
             System.out.println(errorMessage.getMessage(bindingResult.getFieldError(), LocaleContextHolder.getLocale()));
             throw new BadRequestException(errorMessage.getMessage(bindingResult.getFieldError(), LocaleContextHolder.getLocale()));
@@ -67,8 +105,20 @@ public final class PatientController {
         return ResponseEntity.ok(new Message(LocalDate.now(), HttpStatus.OK.value(), "Paziente registrato con successo!"));
     }
 
+    @Operation(summary = "PUT a Patient", description = "Modifica un Paziente registrato nel Database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Il Paziente è stato modificato correttamente", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Il formato della richiesta non è valido", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Il Paziente non è registrato, usare il metodo POST per inserirlo", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            })
+    })
     @PutMapping(path = "/{taxCode}", consumes = MediaType.APPLICATION_JSON_VALUE) @SneakyThrows
-    public ResponseEntity<Message> putPatient(@PathVariable String taxCode, @Valid @RequestBody Patient patient, BindingResult bindingResult) {
+    public ResponseEntity<Message> putPatient(@Parameter(description = "Codice Fiscale del Paziente da modificare") @PathVariable String taxCode, @Parameter(description = "Paziente da modificare") @Valid @RequestBody Patient patient, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
             System.out.println(errorMessage.getMessage(bindingResult.getFieldError(), LocaleContextHolder.getLocale()));
             throw new BadRequestException(errorMessage.getMessage(bindingResult.getFieldError(), LocaleContextHolder.getLocale()));
@@ -82,8 +132,17 @@ public final class PatientController {
         return ResponseEntity.ok(new Message(LocalDate.now(), HttpStatus.OK.value(), "Paziente modificato con successo!"));
     }
 
+    @Operation(summary = "DELETE a Patient by Id", description = "Elimina un Paziente corrispondente al Codice Fiscale passato come parametro")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Il Paziente è stato eliminato correttamente", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            }),
+            @ApiResponse(responseCode = "409", description = "Non esiste alcun Paziente con quel Codice Fiscale registrato nel Database", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Message.class))
+            })
+    })
     @DeleteMapping(path = "/{taxCode}", consumes = MediaType.APPLICATION_JSON_VALUE) @SneakyThrows
-    public ResponseEntity<Message> deletePatientByTaxCode(@PathVariable String taxCode) {
+    public ResponseEntity<Message> deletePatientByTaxCode(@Parameter(description = "Codice Fiscale del Paziente da rimuovere") @PathVariable String taxCode) {
         final Optional<PersonDTO> patientDTO = service.findByTaxCode(taxCode);
         if (patientDTO.isEmpty() || !(patientDTO.get() instanceof PatientDTO)) {
             throw new ConflictException("Paziente non registrato, usare metodo POST per registrarlo!");
