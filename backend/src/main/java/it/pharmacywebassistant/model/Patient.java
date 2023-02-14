327d1bd7a73ab22d1672c69ac30aeb11741afbe7
mrOrange <35071573+mrOranger@users.noreply.github.com>
2023-02-14 16:56:06 +0100

    Add reference OneToMany between Patient and Prescription

--- backend/src/main/java/it/pharmacywebassistant/model/Patient.java
+++ backend/src/main/java/it/pharmacywebassistant/model/Patient.java
@@ -1,5 +1,6 @@
 package it.pharmacywebassistant.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,12 +9,17 @@ import lombok.Setter;
 import java.io.Serial;
 import java.io.Serializable;
 import java.sql.Date;
+import java.util.List;
 
 @Entity(name = "Patient") @Getter @Setter @NoArgsConstructor
 public final class Patient extends Person implements Serializable {
 
     @Serial
     private static final long serialVersionUID = -18274019284732L;
+
+    @OneToMany(mappedBy = "patient")
+    @JsonManagedReference
+    private List<Prescription> prescriptionList;
 
     public Patient(String taxCode, String firstName, String lastName, Date dateOfBirth) {
         super(taxCode, firstName, lastName, dateOfBirth);
