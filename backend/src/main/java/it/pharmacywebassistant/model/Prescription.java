5127d8888ca22adc16ba29efdf43e44965d31490
mrOrange <35071573+mrOranger@users.noreply.github.com>
2023-02-14 16:52:33 +0100

    Created Prescription Entity class

---
+++ backend/src/main/java/it/pharmacywebassistant/model/Prescription.java
@@ -0,0 +1,37 @@
+package it.pharmacywebassistant.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity(name = "Person") @Table(name = "People")
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
+public class Prescription implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -91029471829388L;
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull(message = "{Prescription.Patient.NotNull}")
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "Patient", nullable = false)
+    @JsonBackReference
+    private Patient patient;
+
+    @NotNull(message = "{Prescription.Doctor.NotNull}")
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "Doctor", nullable = false)
+    @JsonBackReference
+    private Doctor doctor;
+
+}
