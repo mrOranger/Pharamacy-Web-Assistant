534c7c8ab380ec18a7611ac4d9414d5543ffadfc
edoardo <edo.oranger@gmail.com>
2023-02-19 13:42:12 +0100

    Add missing "@Service" annotation

--- backend/src/main/java/it/pharmacywebassistant/mapper/CompanyMapper.java
+++ backend/src/main/java/it/pharmacywebassistant/mapper/CompanyMapper.java
@@ -3,10 +3,12 @@ package it.pharmacywebassistant.mapper;
 import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import it.pharmacywebassistant.model.Company;
 import it.pharmacywebassistant.model.dto.CompanyDTO;
 
+@Service
 public class CompanyMapper implements Function<Company, CompanyDTO>{
 	
 	@Autowired
